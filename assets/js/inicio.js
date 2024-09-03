0642998fd88a25ab01919e36706321c5aa916c53
Mauricio Alejandro Campos Villalon <macamposv@ine.gob.cl>
2024-09-03 15:37:52 -0400

    finalizado

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -38,7 +38,7 @@ function validar(evt) {
 
     //    console.log(identidad);
     //    console.log(contrasena);
-
+    let opin = true;
     Object.keys(usuario).forEach(i => {
 
 
@@ -49,41 +49,100 @@ function validar(evt) {
             sal = usuario[i].saldo;
         }
     })
+    while (opin === true) {
+        let op2="";
+        let nwsal=0;
+        if (est1 === true) {
+            alert('Bienvenido a banca en linea \n' + nomb);
+            let op = prompt('Seleccione que desea hacer: \n 1.-Ver saldo.\n 2.-Realizar giro.\n 3.-Realizar deposito. \n 4.-Salir ')
+            
+            switch (op) {
+                case "1":
+                    alert('Su saldo actual es $' + sal );
+                    op2= prompt('Para volver al menu principal presione S \n de lo contrario presione cualquier tecla').toLowerCase()
+                    console.log(op2)
+                    if (op2==='s'){
+                        break;
+                    }else{
+                        opin = false;
+                        break;
+                    }
+                    
 
-    if (est1 === true) {
-        alert('Bienvenido a banca en linea n/' + nomb);
-        let op = prompt('Seleccione que desea hacer: \n 1.-Ver saldo.\n 2.-Realizar giro.\n 3.-Realizar deposito. \n 4.-Salir ')
+                case "2":
+                    nwsal = giro(sal);
+                    console.log(nwsal)
+                    usuario[us].saldo = nwsal;
+                    sal = usuario[us].saldo;
+                    console.log(usuario[us].saldo)
+                    op2= prompt('Para volver al menu principal presione S \n de lo contrario presione cualquier tecla').toLowerCase()
+                    if (op2==='s'){
+                        break;
+                    }else{
+                        opin = false;
+                        break;
+                    }
 
-        switch (op) {
-            case "1":
-                let nwsal=giro(sal);
-                usuario[us].saldo = nwsal;
-                console.log(usuario[us].saldo)
-                break;
-            case "2":
-                senalllamadas();
-                break;
-            case "3":
-                OfertaComercial();
-                break;
-            case "4":
-                break;
-            default:
-                alert('Opcion ingresada no es valida');
+
+                case "3":
+                    nwsal = deposito(sal);
+                    console.log(nwsal)
+                    usuario[us].saldo = nwsal;
+                    sal = usuario[us].saldo;
+                    console.log(usuario[us].saldo)
+                    op2= prompt('Para volver al menu principal presione S \n de lo contrario presione cualquier tecla').toLowerCase()
+                    if (op2==='s'){
+                        break;
+                    }else{
+                        alert('Muchas gracias Hasta Pronto')
+                        opin = false;
+                        break;
+                    }
+                case "4":
+                    opin = false;
+                    break;
+
+                default:
+                    alert('Opcion ingresada no es valida');
+                    opin
+            }
+
+
+        } else {
+            alert(" Usuario no encontrado");
+            opin = false;
+
         }
 
-
-    } else {
-        alert(" Usuario no encontrado")
     }
+
 
 
 }
 
 function giro(sald) {
-    let newsal  = prompt
+    let newsal = parseInt(prompt('Su saldo actual es ' + sald + ' Ingrese el monto que desea girar'))
+    if (sald >= newsal) {
+        sald -= newsal
+        alert('Su nuevo saldo es $'+sald)
+        
+    }
+    else {
+        alert('Operacion no se Puede realizar ya que no tiene saldo disponible para esta Operacion')
+        
+    }
+    return sald;
+}
 
-
-
-
+function deposito(sal) {
+    let depsal = parseInt(prompt('Sulasodo actual es $'+sal+'\n Ingrese el monto que desea depositar'))
+    if (depsal<=0){
+        alert('Operacion No se puede realizar, Monto a depositar debe ser superior a 0.')
+    }
+    else {
+        sal += depsal
+        alert('Deposito realizado su nuevo saldo es $'+sal)
+        
+    }
+    return sal;
 }
